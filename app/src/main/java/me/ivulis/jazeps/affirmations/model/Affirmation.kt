68ca13b74947232703fe80687f2ca9765d96621d
Jazeps Ivulis <jazeps.ivulis@accenture.com>
2023-09-01 17:04:11 +0300

    Implemented logic to display list of images using cards; polished UI

--- app/src/main/java/me/ivulis/jazeps/affirmations/model/Affirmation.kt
+++ app/src/main/java/me/ivulis/jazeps/affirmations/model/Affirmation.kt
@@ -1,4 +1,9 @@
 package me.ivulis.jazeps.affirmations.model
 
-data class Affirmation(val stringResourceId: Int) {
-}
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class Affirmation(
+    @StringRes val stringResourceId: Int,
+    @DrawableRes val imageResourceId: Int
+)
