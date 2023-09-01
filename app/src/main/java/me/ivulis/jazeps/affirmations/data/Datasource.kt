cff0437e11d2773ee617c062181cbef3fd1f520c
Jazeps Ivulis <jazeps.ivulis@accenture.com>
2023-09-01 16:28:08 +0300

    Implemented functionality to display list of strings using RecyclerView

---
+++ app/src/main/java/me/ivulis/jazeps/affirmations/data/Datasource.kt
@@ -0,0 +1,22 @@
+package me.ivulis.jazeps.affirmations.data
+
+import me.ivulis.jazeps.affirmations.R
+import me.ivulis.jazeps.affirmations.model.Affirmation
+
+class Datasource {
+
+    fun loadAffirmations(): List<Affirmation> {
+        return listOf<Affirmation>(
+            Affirmation(R.string.affirmation1),
+            Affirmation(R.string.affirmation2),
+            Affirmation(R.string.affirmation3),
+            Affirmation(R.string.affirmation4),
+            Affirmation(R.string.affirmation5),
+            Affirmation(R.string.affirmation6),
+            Affirmation(R.string.affirmation7),
+            Affirmation(R.string.affirmation8),
+            Affirmation(R.string.affirmation9),
+            Affirmation(R.string.affirmation10)
+        )
+    }
+}
---
+++ app/src/main/java/me/ivulis/jazeps/affirmations/model/Affirmation.kt
@@ -0,0 +1,4 @@
+package me.ivulis.jazeps.affirmations.model
+
+data class Affirmation(val stringResourceId: Int) {
+}
